221789edd92af50788d2631c5dcca5cabc9589f3
Adam Farden <adam@farden.cz>
2016-07-30 10:26:59 +0200

    Initial scripts and local manifests for Copperhead OS

---
+++ bld.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+# $1 is device codename (according to $OUT folder)
+# $2 is specific output target (eg recoveryimage)
+
+XPERIA_DATE=$(date -u +%Y%m%d)
+
+case $1 in
+
+    # Loire Devices
+    "suzu")
+        XPERIA_TARGET=aosp_f5121
+        ;;
+
+    # Kitakami Devices
+    "satsuki")
+        XPERIA_TARGET=aosp_e6853
+        ;;
+    "sumire")
+        XPERIA_TARGET=aosp_e6653
+        ;;
+    "suzuran")
+        XPERIA_TARGET=aosp_e5823
+        ;;
+    "ivy")
+        XPERIA_TARGET=aosp_e6553
+        ;;
+    "karin")
+        XPERIA_TARGET=aosp_sgp771
+        ;;
+    "karin_windy")
+        XPERIA_TARGET=aosp_sgp712
+        ;;
+
+    # Kanuti Devices
+    "tulip")
+        XPERIA_TARGET=aosp_e2303
+        ;;
+
+    # Shinano Devices
+    "leo")
+        XPERIA_TARGET=aosp_d6603
+        ;;
+    "aries")
+        XPERIA_TARGET=aosp_d5803
+        ;;
+    "sirius")
+        XPERIA_TARGET=aosp_d6503
+        ;;
+    "scorpion")
+        XPERIA_TARGET=aosp_sgp621
+        ;;
+    "scorpion_windy")
+        XPERIA_TARGET=aosp_sgp611
+        ;;
+    "castor")
+        XPERIA_TARGET=aosp_sgp521
+        ;;
+    "castor_windy")
+        XPERIA_TARGET=aosp_sgp511
+        ;;
+
+    # Rhine Devices
+    "honami")
+        XPERIA_TARGET=aosp_c6903
+        ;;
+    "amami")
+        XPERIA_TARGET=aosp_d5503
+        ;;
+    "togari")
+        XPERIA_TARGET=aosp_c6833
+        ;;
+
+    *)
+        echo "Wait, what? \"$1\" is not a valid build target!"
+        echo ""
+        exit -1
+    ;;
+esac
+
+CORE_COUNT=`grep processor /proc/cpuinfo | wc -l`
+
+# Initialize environment
+source build/envsetup.sh
+export LANG=C
+unset _JAVA_OPTIONS
+export BUILD_NUMBER=$(date --utc +%Y.%m.%d.%H.%M.%S)
+export DISPLAY_BUILD_NUMBER=true
+export ANDROID_HOME=~/android-sdk-linux/
+
+# Initialize the device
+lunch $XPERIA_TARGET-userdebug
+
+# Make ALL the things!
+make $2 -j$((CORE_COUNT + 2))
+
+if [ -f out/dist/$XPERIA_TARGET-img-*.zip ]; then
+    echo "Copying $1-CopperheadOS-$XPERIA_DATE.zip to ~/IMG"
+    echo ""
+    rm ~/IMG/$1-CopperheadOS-*.zip
+    # Actually we're doing a hard link so we can immediately
+    # remove the $OUT folder and build another device.
+    ln out/dist/$XPERIA_TARGET-img-*.zip ~/IMG/$1-CopperheadOS-$XPERIA_DATE.zip
+fi
